1464d9239d7ff7a5eecdecd3d55a334ebd3426cb
Adhyan-dev <143337953+Adhyan-dev@users.noreply.github.com>
2024-04-30 12:11:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload()
+{
+
+}
+function setup()
+{
+  var canvas = createCanvas(400,400);
+  canvas.center();
+}
+function draw()
+{
+ 
+}
+function takeSnapshot()
+{
+    save('picture.jpg');
+}
